c7b86eaaf4915e2136b711d05054883cf28e0b7e
ahuva12 <ahuvaeshit@gmail.com>
2024-09-17 18:39:44 +0300

    more changes

--- src/logic/weatherService.js
+++ src/logic/weatherService.js
@@ -1,20 +1,28 @@
-
 export const fetchWeatherData = async () => {
-    const cities = ['Eilat', 'London', 'New York', 'Alaska'];
-    const apiKey = '8ee633956bad6ae1965b557a94ecfcba';
-  
-    const data = await Promise.all(
-      cities.map(async (city) => {
+  const cities = ['Eilat', 'London', 'New York', 'Alaska'];
+  const apiKey = '8ee633956bad6ae1965b557a94ecfcba';
+
+  const data = await Promise.all(
+    cities.map(async (city) => {
+      try {
         const response = await fetch(
           `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric&lang=he`
         );
-        if (response.status === 429) {
-          throw new Error('429 - Too many requests');
+
+        if (!response.ok) { 
+          const errorData = await response.json(); 
+          const errorMessage = errorData.message || 'Unknown error';
+          throw new Error(`Error ${response.status}: ${errorMessage}`);
         }
+
         return response.json();
-      })
-    );
-  
-    return data;
-  };
-  
+
+      } catch (error) {
+        console.error(`Error fetching data for ${city}: ${error.message}`);
+        throw error;
+      }
+    })
+  );
+
+  return data;
+};
